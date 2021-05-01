81d9faf501583e8a8539fe5247d6927fc0908150
Maria Grebenkova <grebenkova.m.p@gmail.com>
2021-05-01 18:46:40 +0300

    initial commit

---
+++ src/test/java/AuthTest.java
@@ -0,0 +1,79 @@
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static data.DataGenerator.Registration.getRegisteredUser;
+import static data.DataGenerator.Registration.getUser;
+import static data.DataGenerator.getRandomLogin;
+import static data.DataGenerator.getRandomPassword;
+
+
+public class AuthTest {
+
+    @BeforeEach
+    public void setUpAll() {
+        open("http://localhost:9999");
+    }
+
+
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+        val registeredUser = getRegisteredUser("active");
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $("h2").shouldHave(text("Личный кабинет"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with not registered user")
+    void shouldGetErrorIfNotRegisteredUser() {
+        val notRegisteredUser = getUser("blocked");
+        $("[data-test-id='login'] input").setValue(notRegisteredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(notRegisteredUser.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $("[data-test-id='error-notification']").shouldHave(text("Неверно указан логин или пароль"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with blocked registered user")
+    void shouldGetErrorIfBlockedUser() {
+        val blockedUser = getUser("blocked");
+        $("[data-test-id='login'] input").setValue(blockedUser.getLogin());
+        $("[data-test-id='password'] input").setValue(blockedUser.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $("[data-test-id='error-notification']").shouldHave(text("Неверно указан логин или пароль"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong login")
+    void shouldGetErrorIfWrongLogin() {
+        val registeredUser = getRegisteredUser("active");
+        val wrongLogin = getRandomLogin();
+        $("[data-test-id='login'] input").setValue(wrongLogin);
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $("[data-test-id='error-notification']").shouldHave(text("Неверно указан логин или пароль"));
+    }
+
+
+    @Test
+    @DisplayName("Should get error message if login with wrong password")
+    void shouldGetErrorIfWrongPassword() {
+        val registeredUser = getRegisteredUser("active");
+        val wrongPassword = getRandomPassword();
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(wrongPassword);
+        $$("button").find(exactText("Продолжить")).click();
+        $("[data-test-id='error-notification']").shouldHave(text("Неверно указан логин или пароль"));
+    }
+
+}
+
+
+
+
